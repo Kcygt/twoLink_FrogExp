ff59a8d1cfd1aaaafb183501ad47a58dd6223988
Koray Can <92692546+Kcygt@users.noreply.github.com>
2024-12-20 15:02:46 +0000

    seperate the code as Norm and Filt

--- mySquareCodeTESTTT.m
+++ mySquareCodeTESTTT.m
@@ -7,31 +7,52 @@ g = 0;
 tspan = [0 20]; % Total simulation time
 
 % Prefilter
-Wn = 30;
+Wn = 20;
 sigma = 1;
 num = Wn^2;
 den = [1 2*sigma*Wn Wn^2];
 Prefilter = tf(num,den);
 
 % Desired Joint Trajectory
-qDes = [-0.4240, 2.4189;
-        0.1296,  1.9552;   % MAIN
-        0.0,     1.5708;   % MAIN
-       -0.5139,  1.9552;   % MAIN
-       -0.4240,  2.4189 ]; % MAIN
+qDes = [    -0.4240,   2.4189; % MAIN
 
-xi = linspace(1, size(qDes, 1), size(qDes, 1) * 1000 + 1); % Interpolation indices
-qDes = [interp1(1:size(qDes, 1), qDes(:,1), xi, 'linear')', ...
-        interp1(1:size(qDes, 1), qDes(:,2), xi, 'linear')'];
+    % -0.2936    2.3392 ;% second middle
+    % -0.2       2.26;
+    -0.1205    2.2065 ;% first middle
+    % -0.04      2.13;
+    % 0.0337     2.0601; % second middle
+    % 0.07       2;
+    0.1296    1.9552; % MAIN
+    % 0.1       1.92;
+    % 0.0821     1.8965 ;% second middle
+    % 0.055      1.84;
+    0.0316     1.7913 ;% first middle
+    % 0.015      1.72;
+    % 0.0050     1.6659 ;% second middle
+    % 0.002      1.62;
 
+    0.0,       1.5708; % MAIN
+    % -0.1002    1.6659;% second middle
+    -0.2522    1.7913 ;% first middle
+    % -0.4078    1.8965;% second middle
+    -0.5139   1.9552; % MAIN
+    % -0.5229    2.0601 ;%  second middle
+    -0.5153    2.2065 ;% first middle
+    % -0.4749    2.3392; %  second middle
+    -0.4240,   2.4189 % MAIN
+    ]; % Modify as needed
 
-t = linspace(0, tspan(2), length(qDes)); % Time vector
-qDesF = [lsim(Prefilter, qDes(:,1), t, qDes(1,1)), lsim(Prefilter, qDes(:,2), t, qDes(1,2))];
+% qDes, qDesF and change the first 300 value with the home  position
+qDes = repelem(qDes, 400, 1);
+tNorm = linspace(0, tspan(2), length(qDes)); % Time vector
+tFilt = linspace(0, tspan(2), length(qDes)); % Time vector
 
-step = 50;
+qDesF = [lsim(Prefilter, qDes(:,1), tNorm, qDes(1,1)), lsim(Prefilter, qDes(:,2), tNorm, qDes(1,2))];
+step = 300;
 qDesF(1:step,:) = [ones(step,1) * -0.4240, ones(step,1)*2.4189];  % it start from [0,0]
 
-xDes = forward_kinematics(qDesF(:, 1), qDesF(:, 2), l1, l2);
+xDesNorm = forward_kinematics(qDes(:, 1), qDes(:, 2), l1, l2);
+xDesFilt = forward_kinematics(qDesF(:, 1), qDesF(:, 2), l1, l2);
 
 % Controller gains
 K = 60; % Proportional gain
@@ -43,24 +64,35 @@ qd0 = [0; 0]; % Initial joint velocities
 initial_state = [q0; qd0];
 
 % Solve the ODE
-[t, state] = ode45(@(t, x) robot_dynamics(t, x, l1, l2, m1, m2, g, K, B, qDesF, tspan), tspan, initial_state);
+[tNormOut, stateNorm] = ode45(@(tNorm, x) robot_dynamics(tNorm, x, l1, l2, m1, m2, g, K, B, qDes, tspan), tspan, initial_state);
+[tFiltOut, stateFilt] = ode45(@(tFilt, x) robot_dynamics(tFilt, x, l1, l2, m1, m2, g, K, B, qDesF, tspan), tspan, initial_state);
 
 % Extract results
-qPos = state(:, 1:2); % Joint positions
-qVel = state(:, 3:4); % Joint velocities
-xAct = forward_kinematics(qPos(:, 1), qPos(:, 2), l1, l2);
-xVelJ = zeros(size(xAct));
+qPosNorm = stateNorm(:, 1:2); % Joint positions
+qVelNorm = stateNorm(:, 3:4); % Joint velocities
+qPosFilt = stateFilt(:, 1:2); % Joint positions
+qVelFilt = stateFilt(:, 3:4); % Joint velocities
 
-for i=1:length(t)
-    xVelJ(i,:) = jacobian_2link(qPos(i,1),qPos(i,2),1,1) * qVel(i,:)';
+
+xActNorm = forward_kinematics(qPosNorm(:, 1), qPosNorm(:, 2), l1, l2);
+xActFilt = forward_kinematics(qPosFilt(:, 1), qPosFilt(:, 2), l1, l2);
+
+xVelJNorm = zeros(size(xActNorm));
+xVelJFilt = zeros(size(xActFilt));
+
+for i=1:length(qPosNorm)
+    xVelJNorm(i,:) = jacobian_2link(qPosNorm(i,1),qPosNorm(i,2),1,1) * qVelNorm(i,:)';
+    xVelJFilt(i,:) = jacobian_2link(qPosFilt(i,1),qPosFilt(i,2),1,1) * qVelFilt(i,:)';
+
 end
 
 
 % Plot trajectory
 figure(1); hold on; grid on;
-plot(xDes(:, 1), xDes(:, 2), 'o', 'DisplayName', 'End Effector Desired');
+plot(xDesNorm(:, 1), xDesNorm(:, 2), 'o', 'DisplayName', 'End Effector Desired');
+plot(xActNorm(:, 1), xActNorm(:, 2), '-', 'DisplayName', 'End Effector Actual');
+plot(xActFilt(:, 1), xActFilt(:, 2), '*', 'DisplayName', 'End Effector Actual');
 
-plot(xAct(:, 1), xAct(:, 2), '-', 'DisplayName', 'End Effector Actual');
 xlabel('X Position');
 ylabel('Y Position');
 title('Cartesian Space Trajectory Following');
@@ -70,9 +102,12 @@ legend show;
 % quiver(xAct(:,1),xAct(:,2),xVel(:,1),xVel(:,2))
 
 figure(3); hold on; grid on;
-plot(t,xVelJ(:,1))
+plot(tNormOut,xVelJNorm(:,1))
+plot(tFiltOut,xVelJFilt(:,1))
+
 figure(4); hold on; grid on;
-plot(t,xVelJ(:,2))
+plot(tNormOut,xVelJNorm(:,2))
+plot(tFiltOut,xVelJFilt(:,2))
 
 % Functions
 function dxdt = robot_dynamics(t, x, l1, l2, m1, m2, g, K, B, Q, tspan)
