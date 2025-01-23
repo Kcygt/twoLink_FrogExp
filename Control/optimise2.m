90a47d3d3769625fb768552ab2f66011b12da774
Koray Can <92692546+Kcygt@users.noreply.github.com>
2025-01-23 13:21:09 +0000

    upload

--- Control/optimise2.m
+++ Control/optimise2.m
@@ -8,11 +8,13 @@ qDes = inverse_kinematics(cartesianX, cartesianY, 1, 1)';  % Desired joint angle
 % Optimization setup
 initParams = [3, 6, 9, 12, 15,     1   20,   45]; % Initial guess for [time, wn, bj, kj]
 % initialParams =   [  1.9142    3.3451    4.0022    6.0404    8.0008    4.8515   10.4579   47.2359];
+initParams = [    1.7300    3.9999    5.0020    6.0017    8.0005    4.5564   12.3538   49.7463
+]; % Initial guess for [time, wn, bj, kj]
 
 [init_T, init_Y] = ode45(@(t, x) myTwolinkwithprefilter(t, x, initParams(6), initParams(1:5), qDes, initParams(7), initParams(8)), [0 initParams(5)], zeros(8, 1));
 
 % Lower and upper boundaries 
-tStep = 1;
+tStep = 2;
 lb = [0,     tStep,   2*tStep, 3*tStep, 4*tStep,   0.1,    1,  10  ];   % Lower bounds
 ub = [tStep, 2*tStep, 3*tStep, 4*tStep, 5*tStep,    5,   50,  100 ];     % Upper bounds
 
@@ -62,8 +64,8 @@ function error = objectiveFunction(params, qDes)
     distto3 = w1 * sum((y(:,5:6) - qDes(3,:)).^2,2) + w2 * sum(abs(time(3) - t));
     distto4 = w1 * sum((y(:,5:6) - qDes(4,:)).^2,2) + w2 * sum(abs(time(4) - t));
     distto5 = w1 * sum((y(:,5:6) - qDes(5,:)).^2,2) + w2 * sum(abs(time(5) - t));
-    % error   = min(distto1) + min(distto2) + min(distto3) + min(distto4) + min(distto5);
-    error = min(distto3);
+    error   = min(distto1) + min(distto2) + min(distto3) + min(distto4) + min(distto5);
+    % error   =min(distto3);
 end
 
 % myTwolinkwithprefilter function
